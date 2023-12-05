ebcbab4ed7da3aee5e7fb69979d74e202df6b7bc
zakrnem <zakrnem@zohomail.com>
2023-12-05 06:56:48 -0600

    Prevent empty spec fields from being submitted

--- public/javascripts/addSpec.js
+++ public/javascripts/addSpec.js
@@ -1,25 +1,43 @@
 const specList = document.querySelector(".spec-list");
-const specsArray = [...specList.children]
-let specCount = (specsArray.length > 2) ? specsArray.length-1 : 1 
+const specsArray = [...specList.children];
+let specCount = specsArray.length > 2 ? specsArray.length - 1 : 1;
 
 specList.addEventListener("click", (e) => {
   e.preventDefault();
 
   if (e.target.id === "add-spec") {
     const prevAddSpecBtn = document.getElementById("add-spec");
-    specList.removeChild(prevAddSpecBtn);
+    const prevInput = document.getElementById(`spec${specCount}`);
+    const errorsDiv = document.querySelector(".errors");
+    let prevErrorMsg = document.querySelector("#empty-spec-error");
 
-    specCount++
-    const input = document.createElement("input");
-    input.id = `spec${specCount}`;
-    input.type = "text";
-    input.name = `specification`;
-    input.placeholder = `Specification #${specCount}`;
+    if (prevInput.value !== "") {
+      prevInput.style.border = "";
+      if (prevErrorMsg) errorsDiv.removeChild(prevErrorMsg);
+      specList.removeChild(prevAddSpecBtn);
 
-    const insertSpec = document.createElement("button");
-    insertSpec.textContent = "+Add specification";
-    insertSpec.id = `add-spec`;
+      specCount++;
+      const input = document.createElement("input");
+      input.id = `spec${specCount}`;
+      input.type = "text";
+      input.name = `specification`;
+      input.placeholder = `Specification #${specCount}`;
 
-    specList.append(input, insertSpec);
+      const insertSpec = document.createElement("button");
+      insertSpec.textContent = "+Add specification";
+      insertSpec.id = `add-spec`;
+
+      specList.append(input, insertSpec);
+    } else {
+      let errorMsg = document.createElement("p");
+      errorMsg.id = "empty-spec-error";
+      errorMsg.textContent =
+        "Please fill in the current specification before adding another.";
+
+      if (!prevErrorMsg) {
+        errorsDiv.append(errorMsg);
+        prevInput.style.border = "solid 2px red";
+      }
+    }
   }
 });
