347a53bb73c8d5c113d6dd0078913897f5913d98
zakrnem <zakrnem@zohomail.com>
2023-12-07 20:10:16 -0600

    Implement item instance form GET

--- controllers/iteminstanceController.js
+++ controllers/iteminstanceController.js
@@ -1,16 +1,18 @@
 const ItemInstance = require("../models/iteminstance");
-const Item = require("../models/item")
-const Location = require("../models/location")
+const Item = require("../models/item");
+const Location = require("../models/location");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const admin = true;
 
 exports.iteminstance_list = asyncHandler(async (req, res, next) => {
-  const allIteminstances = await ItemInstance.find({}).populate("item").populate("location");
+  const allIteminstances = await ItemInstance.find({})
+    .populate("item")
+    .populate("location");
   allIteminstances.forEach((instance) => {
-    instance.item.name = decodeURIComponent(instance.item.name)
-    instance.location.name = decodeURIComponent(instance.location.name)
-  })
+    instance.item.name = decodeURIComponent(instance.item.name);
+    instance.location.name = decodeURIComponent(instance.location.name);
+  });
 
   res.render("iteminstance_list", {
     title: "Product Instance List",
@@ -20,7 +22,22 @@ exports.iteminstance_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.iteminstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED, Item instance create GET");
+  const allItems = await Item.find({})
+  const allLocations = await Location.find({})
+  allItems.forEach((item) => {
+    item.name = decodeURIComponent(item.name)
+  })
+  allLocations.forEach((location) => {
+    location.name = decodeURIComponent(location.name)
+  })
+
+  res.render("iteminstance_form", {
+    title: "Create product instance",
+    item_list: allItems,
+    item_instance: false,
+    location_list: allLocations,
+    admin: admin,
+  })
 });
 
 exports.iteminstance_create_post = asyncHandler(async (req, res, next) => {
@@ -44,14 +61,20 @@ exports.iteminstance_delete_post = asyncHandler(async (req, res, next) => {
 });
 
 exports.iteminstance_detail = asyncHandler(async (req, res, next) => {
-  const iteminstanceDetails = await ItemInstance.findById(req.params.id).populate('item').populate("location");
-  
-  iteminstanceDetails.item.name = decodeURIComponent(iteminstanceDetails.item.name)
-  iteminstanceDetails.location.name = decodeURIComponent(iteminstanceDetails.location.name)
+  const iteminstanceDetails = await ItemInstance.findById(req.params.id)
+    .populate("item")
+    .populate("location");
 
-  res.render('iteminstance_detail', 
-  { title: 'Product instance detail',
-  iteminstance_detail: iteminstanceDetails,
-  admin: admin,
-  })
-})
+  iteminstanceDetails.item.name = decodeURIComponent(
+    iteminstanceDetails.item.name,
+  );
+  iteminstanceDetails.location.name = decodeURIComponent(
+    iteminstanceDetails.location.name,
+  );
+
+  res.render("iteminstance_detail", {
+    title: "Product instance detail",
+    iteminstance_detail: iteminstanceDetails,
+    admin: admin,
+  });
+});
