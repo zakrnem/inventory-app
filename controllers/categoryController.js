b03d036577615fec0a0b0a2981c89a6a40068f90
zakrnem <zakrnem@zohomail.com>
2023-12-12 13:30:53 -0600

    Implement category create functionality

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,6 +4,7 @@ const ItemInstance = require("../models/iteminstance");
 const Location = require("../models/location");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
+const mongoose = require("mongoose");
 const admin = true;
 
 exports.category_list = asyncHandler(async (req, res, next) => {
@@ -16,12 +17,47 @@ exports.category_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED, Category create GET");
+  res.render("category_form", {
+    title: "Create category",
+    category: false,
+    errors: [],
+    admin: admin,
+  })
 });
 
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED, Category create POST");
-});
+exports.category_create_post = [
+  body("name", "Title must not be empty.").trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    req.body.name = encodeURIComponent(req.body.name);
+    next();
+  },
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const category = new Category({ name: req.body.name });
+
+    const existingCategory = await Category.findOne({
+      name: req.body.name,
+    });
+    const existingError = existingCategory !== null;
+
+    if (!errors.isEmpty() || existingError) {
+      res.render("category_form", {
+        title: "Create category",
+        category: category,
+        errors: errors.array(),
+        admin: admin,
+      });
+      return;
+    } else {
+      await category.save();
+      res.redirect(category.url);
+    }
+  })
+]
+
+
 
 exports.category_update_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED, Category update GET");
