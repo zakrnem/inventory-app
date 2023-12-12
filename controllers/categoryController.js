d180f8abc1c584909b01edf0f4c671d35f9e7b72
zakrnem <zakrnem@zohomail.com>
2023-12-11 20:16:44 -0600

    Implement categories list page

---
+++ controllers/categoryController.js
@@ -0,0 +1,40 @@
+const Category = require("../models/category");
+const Item = require("../models/item");
+const ItemInstance = require("../models/iteminstance");
+const Location = require("../models/location");
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const admin = true;
+
+exports.category_list = asyncHandler(async (req, res, next) => {
+    const allCategories = await Category.find({}, "name");
+    res.render("category_list", { title: "Categories list", category_list: allCategories, admin: admin });
+  });
+  
+  exports.category_create_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED, Category create GET");
+  });
+  
+  exports.category_create_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED, Category create POST");
+  });
+  
+  exports.category_update_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED, Category update GET");
+  });
+  
+  exports.category_update_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED, Category update POST");
+  });
+  
+  exports.category_delete_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED, Category delete GET");
+  });
+  
+  exports.category_delete_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED, Category delete POST");
+  });
+  
+  exports.category_detail = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED, Category detail");
+  })
--- routes/catalog.js
+++ routes/catalog.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const item_controller = require("../controllers/itemController");
 const iteminstance_controller = require("../controllers/iteminstanceController");
+const category_controller = require("../controllers/categoryController");
 
 // ITEM ROUTES
 
@@ -57,5 +58,23 @@ router.post(
 );
 
 router.get("/iteminstance/:id", iteminstance_controller.iteminstance_detail);
+
+// CATEGORY ROUTES
+
+router.get("/categories", category_controller.category_list);
+
+router.get("/category/create", category_controller.category_create_get);
+
+router.post("/category/create", category_controller.category_create_post);
+
+router.get("/category/:id/delete", category_controller.category_delete_get);
+
+router.post("/category/:id/delete", category_controller.category_delete_post);
+
+router.get("/category/:id/update", category_controller.category_update_get);
+
+router.post("/category/:id/update", category_controller.category_update_post);
+
+router.get("/category/:id", category_controller.category_detail);
 
 module.exports = router;
