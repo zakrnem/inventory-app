72280d821f887b47825bfbbf370250760e415a5a
zakrnem <zakrnem@zohomail.com>
2023-12-16 09:23:06 -0600

    Refactor itemController

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -27,7 +27,7 @@ exports.item_create_get = asyncHandler(async (req, res, next) => {
   res.render("item_form", {
     title: "Create product",
     categories: allCategories,
-    item: false,
+    item_detail: false,
     errors: [],
     existing_error: false,
     admin: admin,
@@ -47,16 +47,11 @@ exports.item_create_post = [
     req.body.specification = req.body.specification.filter(
       (item) => item !== "",
     );
-    next();
-  },
-
-  (req, res, next) => {
     req.body.name = encodeURIComponent(req.body.name);
     req.body.description = encodeURIComponent(req.body.description);
     next();
   },
 
-  // Validate and sanitize fields.
   body("name", "Title must not be empty.").trim().isLength({ min: 1 }).escape(),
   body("description").trim().escape(),
   body("category.*").escape(),
@@ -64,14 +59,8 @@ exports.item_create_post = [
   body("price", "Price must be a valid number.").exists().isFloat({ min: 0 }),
 
   asyncHandler(async (req, res, next) => {
-    // Extract the validation errors from a request.
     const errors = validationResult(req);
-    const allCategories = await Category.find({});
-    allCategories.forEach(
-      (category) => (category.name = decode(decodeURIComponent(category.name))),
-    );
 
-    // Create Item object with escaped and trimmed data
     const item = new Item({
       name: req.body.name,
       description: req.body.description,
@@ -80,24 +69,29 @@ exports.item_create_post = [
       price: req.body.price,
     });
 
-    item.name = decode(decodeURIComponent(item.name));
-    if (item.description)
-      item.description = decode(decodeURIComponent(item.description));
-    if (item.specifications)
-      item.specifications = item.specifications.map((spec) =>
-        decode(decodeURIComponent(spec)),
-      );
-
     const existingInstance = await Item.findOne({
       name: decode(decodeURIComponent(req.body.name)),
     });
     const existingError = existingInstance !== null;
 
     if (!errors.isEmpty() || existingError) {
-      // There are errors. Render form again with sanitized values/errors messages.
+      const allCategories = await Category.find({});
+      allCategories.forEach(
+        (category) =>
+          (category.name = decode(decodeURIComponent(category.name))),
+      );
+
+      item.name = decode(decodeURIComponent(item.name));
+      if (item.description)
+        item.description = decode(decodeURIComponent(item.description));
+      if (item.specifications)
+        item.specifications = item.specifications.map((spec) =>
+          decode(decodeURIComponent(spec)),
+        );
+
       res.render("item_form", {
         title: "Create product",
-        item: item,
+        item_detail: item,
         categories: allCategories,
         admin: admin,
         errors: errors.array(),
@@ -105,26 +99,25 @@ exports.item_create_post = [
       });
       return;
     } else {
-      // Data from form is valid.
-
-      // Save item.
       await item.save();
-      // Redirect to new item record.
       res.redirect(item.url);
     }
   }),
 ];
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-  const [allCategories, item] = await Promise.all([
+  const [allCategories, itemDetails] = await Promise.all([
     Category.find({}),
     Item.findById(req.params.id).populate("category"),
   ]);
 
-  item.name = decode(decodeURIComponent(item.name));
-  if (item.description) item.description = decode(decodeURIComponent(item.description));
-  if (item.specifications)
-    item.specifications = item.specifications.map((spec) =>
+  itemDetails.name = decode(decodeURIComponent(itemDetails.name));
+  if (itemDetails.description)
+    itemDetails.description = decode(
+      decodeURIComponent(itemDetails.description),
+    );
+  if (itemDetails.specifications)
+    itemDetails.specifications = itemDetails.specifications.map((spec) =>
       decode(decodeURIComponent(spec)),
     );
   allCategories.forEach(
@@ -134,7 +127,7 @@ exports.item_update_get = asyncHandler(async (req, res, next) => {
   res.render("item_form", {
     title: "Update product",
     categories: allCategories,
-    item: item,
+    item_detail: itemDetails,
     errors: [],
     existing_error: false,
     admin: admin,
@@ -142,44 +135,31 @@ exports.item_update_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_update_post = [
-  // Convert the specification to an array.
   (req, res, next) => {
     if (!(req.body.specification instanceof Array)) {
       if (typeof req.body.specification === "undefined")
         req.body.specification = [];
       else req.body.specification = new Array(req.body.specification);
     }
-    req.body.specification = req.body.specification.map((item) =>
-      encodeURIComponent(item),
+    req.body.specification = req.body.specification.map((spec) =>
+      encodeURIComponent(spec),
     );
     req.body.specification = req.body.specification.filter(
-      (item) => item !== "",
+      (spec) => spec !== "",
     );
-    next();
-  },
-
-  (req, res, next) => {
     req.body.name = encodeURIComponent(req.body.name);
     req.body.description = encodeURIComponent(req.body.description);
     next();
   },
 
-  // Validate and sanitize fields.
   body("name", "Title must not be empty.").trim().isLength({ min: 1 }).escape(),
   body("description").trim().escape(),
   body("category.*").escape(),
   body("specification.*").escape(),
-  body("price", "Price can't be a negative number.")
-    .exists()
-    .isFloat({ min: 0, max: 30000 }),
+  body("price", "Price must be a valid number.").exists().isFloat({ min: 0 }),
 
   asyncHandler(async (req, res, next) => {
-    // Extract the validation errors from a request.
     const errors = validationResult(req);
-    const allCategories = await Category.find({});
-    allCategories.forEach((category) => (category.name = decode(decodeURIComponent(category.name))));
-
-    // Create Item object with escaped and trimmed data
     const itemCategory = await Category.findById(req.body.category);
     const itemName = decode(decodeURIComponent(req.body.name));
     const item = new Item({
@@ -188,15 +168,8 @@ exports.item_update_post = [
       category: itemCategory,
       specifications: req.body.specification,
       price: req.body.price,
-      _id: req.params.id, // This is required, or a new ID will be assigned!
+      _id: req.params.id,
     });
-
-    if (item.description)
-      item.description = decode(decodeURIComponent(item.description));
-    if (item.specifications)
-      item.specifications = item.specifications.map((spec) =>
-        decode(decodeURIComponent(spec)),
-      );
 
     const existingItem = await Item.find({ name: itemName });
     const prevValue = await Item.findById(req.params.id);
@@ -204,9 +177,22 @@ exports.item_update_post = [
     const existingError = existingItem.length > 0 && !editPrevItem;
 
     if (!errors.isEmpty() || existingError) {
+      const allCategories = await Category.find({});
+      allCategories.forEach(
+        (category) =>
+          (category.name = decode(decodeURIComponent(category.name))),
+      );
+
+      if (item.description)
+        item.description = decode(decodeURIComponent(item.description));
+      if (item.specifications)
+        item.specifications = item.specifications.map((spec) =>
+          decode(decodeURIComponent(spec)),
+        );
+
       res.render("item_form", {
         title: "Update product",
-        item: item,
+        item_detail: item,
         categories: allCategories,
         admin: admin,
         errors: errors.array(),
@@ -228,12 +214,13 @@ exports.item_delete_get = asyncHandler(async (req, res, next) => {
 
   itemDetails.name = decode(decodeURIComponent(itemDetails.name));
   if (itemDetails.description)
-    itemDetails.description = decode(decodeURIComponent(itemDetails.description));
+    itemDetails.description = decode(
+      decodeURIComponent(itemDetails.description),
+    );
   if (itemDetails.specifications)
     itemDetails.specifications = itemDetails.specifications.map((spec) =>
-      decode(decodeURIComponent(spec),
-    ));
-
+      decode(decodeURIComponent(spec)),
+    );
   itemInstances.location = decode(decodeURIComponent(itemInstances));
 
   res.render("item_delete", {
@@ -245,23 +232,31 @@ exports.item_delete_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_delete_post = asyncHandler(async (req, res, next) => {
-  // Get details of item and all their instances (in parallel)
-  const [item, itemInstances] = await Promise.all([
+  const [itemDetails, itemInstances] = await Promise.all([
     Item.findById(req.params.id).exec(),
     ItemInstance.find({ item: req.params.id }).exec(),
   ]);
 
   if (itemInstances.length > 0) {
-    // Item has instances. Render in same way as for GET route.
+    itemDetails.name = decode(decodeURIComponent(itemDetails.name));
+    if (itemDetails.description)
+      itemDetails.description = decode(
+        decodeURIComponent(itemDetails.description),
+      );
+    if (itemDetails.specifications)
+      itemDetails.specifications = itemDetails.specifications.map((spec) =>
+        decode(decodeURIComponent(spec)),
+      );
+    itemInstances.location = decode(decodeURIComponent(itemInstances));
+
     res.render("item_delete", {
       title: "Delete Product",
-      item: item,
+      item_detail: itemDetails,
       item_instances: itemInstances,
       admin: admin,
     });
     return;
   } else {
-    // Item has no instances. Delete object and redirect to the list of items.
     await Item.findByIdAndDelete(req.body.itemid);
     res.redirect("/catalog");
   }
@@ -275,7 +270,9 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 
   itemDetails.name = decode(decodeURIComponent(itemDetails.name));
   if (itemDetails.description)
-    itemDetails.description = decode(decodeURIComponent(itemDetails.description));
+    itemDetails.description = decode(
+      decodeURIComponent(itemDetails.description),
+    );
   if (itemDetails.specifications)
     itemDetails.specifications = itemDetails.specifications.map((spec) =>
       decode(decodeURIComponent(spec)),
@@ -283,10 +280,11 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
   if (itemDetails.category) {
     itemDetails.category.name = decode(
       decodeURIComponent(itemDetails.category.name),
-    )};
+    );
+  }
   itemInstances.forEach((instance) => {
-    instance.location.name = decode(decodeURIComponent(instance.location.name))
-  })
+    instance.location.name = decode(decodeURIComponent(instance.location.name));
+  });
 
   res.render("item_detail", {
     title: "Product detail",
