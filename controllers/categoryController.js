48198e6056c522964b9527ee5b89dfccad55db90
zakrnem <zakrnem@zohomail.com>
2023-12-17 19:43:16 -0600

    Refactor categoryController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,6 @@
 const Category = require("../models/category");
 const Item = require("../models/item");
 const ItemInstance = require("../models/iteminstance");
-const Location = require("../models/location");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const mongoose = require("mongoose");
@@ -33,15 +32,10 @@ exports.category_create_get = asyncHandler(async (req, res, next) => {
 exports.category_create_post = [
   body("name", "Title must not be empty.").trim().isLength({ min: 1 }).escape(),
 
-  (req, res, next) => {
-    req.body.name = encodeURIComponent(req.body.name);
-    next();
-  },
-
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
+    req.body.name = encodeURIComponent(req.body.name);
     const category = new Category({ name: req.body.name });
-
     const existingCategory = await Category.findOne({
       name: req.body.name,
     });
@@ -79,29 +73,24 @@ exports.category_update_get = asyncHandler(async (req, res, next) => {
 exports.category_update_post = [
   body("name", "Title must not be empty.").trim().isLength({ min: 1 }).escape(),
 
-  (req, res, next) => {
-    req.body.name = encodeURIComponent(req.body.name);
-    next();
-  },
-
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
+    req.body.name = encodeURIComponent(req.body.name);
     const category = new Category({
       name: req.body.name,
       _id: req.params.id,
     });
 
-    // Prevent instance duplication
     const [existingCategory, prevValue] = await Promise.all([
       Category.findOne({
         name: req.body.name,
       }),
       Category.findById(req.params.id),
     ]);
-    const existingError = existingCategory !== null;
     const editPrevInstance = prevValue.name.toString() === req.body.name;
+    const existingError = existingCategory !== null && !editPrevInstance;
 
-    if (!errors.isEmpty() || (existingError && !editPrevInstance)) {
+    if (!errors.isEmpty() || existingError) {
       res.render("category_form", {
         title: "Update category",
         category: category,
@@ -118,35 +107,35 @@ exports.category_update_post = [
 ];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
-  const [category, itemList] = await Promise.all([
+  const [category, itemInstanceList] = await Promise.all([
     Category.findById(req.params.id).exec(),
     ItemInstance.find({ item: req.params.id }).exec(),
   ]);
-  category.name = decodeURIComponent(category.name);
-  itemList.forEach((item) => {
-    item.name = decodeURIComponent(item.name);
-    item.description = decodeURIComponent(item.description);
+  category.name = decode(decodeURIComponent(category.name));
+  itemInstanceList.forEach((instance) => {
+    instance.name = decode(decodeURIComponent(instance.name));
+    instance.description = decode(decodeURIComponent(instance.description));
   });
 
   res.render("category_delete", {
     title: "Delete category: ",
     category: category,
-    item_list: itemList,
+    item_instance_list: itemInstanceList,
     admin: admin,
   });
 });
 
 exports.category_delete_post = asyncHandler(async (req, res, next) => {
-  const [category, itemList] = await Promise.all([
+  const [category, itemInstanceList] = await Promise.all([
     Category.findById(req.params.id).exec(),
     ItemInstance.find({ item: req.params.id }).exec(),
   ]);
 
-  if (itemList.length > 0) {
-    res.render("item_delete", {
+  if (itemInstanceList.length > 0) {
+    res.render("category_delete", {
       title: "Delete category: ",
       category: category,
-      item_list: itemList,
+      item_instance_list: itemInstanceList,
       admin: admin,
     });
     return;
@@ -157,21 +146,20 @@ exports.category_delete_post = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  const [category, itemList] = await Promise.all([
+  const [category, itemInstanceList] = await Promise.all([
     Category.findById(req.params.id).exec(),
     Item.find({ category: req.params.id }).exec(),
   ]);
-  category.name = decodeURIComponent(category.name);
-  category.name = decode(category.name);
-  itemList.forEach((item) => {
-    item.name = decodeURIComponent(item.name);
-    item.description = decodeURIComponent(item.description);
+  category.name = decode(decodeURIComponent(category.name));
+  itemInstanceList.forEach((instance) => {
+    instance.name = decode(decodeURIComponent(instance.name));
+    instance.description = decode(decodeURIComponent(instance.description));
   });
 
   res.render("category_detail", {
     title: "Category: ",
     category: category,
-    item_list: itemList,
+    item_instance_list: itemInstanceList,
     admin: admin,
   });
 });
